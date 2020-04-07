3e844d9558a95ffa269fdd6a49a7cd232e354ba1
Tigran <tig.vardanyan.001@gmail.com>
2020-04-08 01:39:14 +0400

    homepage 99%

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,31 @@
 //let photoAvatar = document.getElementById("photo-avatar");
+let root = document.documentElement;
+let colors = document.getElementById('colors');
+colors.addEventListener('click', (e) => {
+  let color = e.target.getAttribute("data-color");
+  localStorage.setItem('targetColor', color);
+  root.style.setProperty('--accent-color', color);
+
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+
+});
+
+function initCChanger() {
+  let toolbarColors = document.querySelectorAll(".page_color-colors div");
+  if(localStorage.getItem('targetColor')) {
+  } else {
+    localStorage.setItem('targetColor' , "#2196F3");
+  }
+  ToolbarUncheck(toolbarColors);
+  document.querySelector(`[data-color = "${localStorage.getItem('targetColor')}"]`);
+  root.style.setProperty('--accent-color', localStorage.getItem('targetColor'));
+}
+
+function ToolbarUncheck(toolbarColors) {
+  for(var i = 0; i < toolbarColors.length; i++) {
+    toolbarColors[i].innerHTML = "";   //uncheck
+  }
+}
+initCChanger();
