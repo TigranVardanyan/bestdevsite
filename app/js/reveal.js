e951ead59f15ff55e567b1715e9d5ce4bf2706bc
Tigran <tig.vardanyan.001@gmail.com>
2020-04-09 02:12:51 +0400

    homepage end

---
+++ app/js/reveal.js
@@ -0,0 +1,11 @@
+const text = baffle('.reveal');
+function loop() {
+  text.set({
+    characters: "▒░▓░ ▓▒<▒/ ░▓░█ █>/ /▓▒░ ░▒█▒",
+    speed: 120
+  });
+  text.start();
+  text.reveal(2500);
+}
+setTimeout(loop, 2500)
+setInterval(loop , 8000)
--- app/js/script.js
+++ app/js/script.js
@@ -1,31 +1,77 @@
 //let photoAvatar = document.getElementById("photo-avatar");
-let root = document.documentElement;
-let colors = document.getElementById('colors');
-colors.addEventListener('click', (e) => {
-  let color = e.target.getAttribute("data-color");
-  localStorage.setItem('targetColor', color);
-  root.style.setProperty('--accent-color', color);
+/*
+ColorChange
+ */
+let root = document.documentElement; // get root
+let colors = document.getElementById('colors'); // html elem all colors wrapper
+let toolbarColors = document.querySelectorAll(".page_color-colors div"); // get all colors
 
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-
-});
-
-function initCChanger() {
-  let toolbarColors = document.querySelectorAll(".page_color-colors div");
-  if(localStorage.getItem('targetColor')) {
-  } else {
-    localStorage.setItem('targetColor' , "#2196F3");
+function ToolbarUncheck(toolbarColors) {
+  for(let i = 0; i < toolbarColors.length; i++) {
+    toolbarColors[i].innerHTML = "";   //uncheck
   }
+} // toolbar uncheck
+function initCChanger() { //init ColorChanger
   ToolbarUncheck(toolbarColors);
   document.querySelector(`[data-color = "${localStorage.getItem('targetColor')}"]`);
   root.style.setProperty('--accent-color', localStorage.getItem('targetColor'));
-}
-
-function ToolbarUncheck(toolbarColors) {
-  for(var i = 0; i < toolbarColors.length; i++) {
-    toolbarColors[i].innerHTML = "";   //uncheck
+  if(localStorage.getItem('targetColor')) { // if any color in local storage
+    document.querySelector(`[data-color = "${localStorage.getItem('targetColor')}"]`).innerHTML = "<i class=\"fas fa-check\"></i>";
+  } else {
+    localStorage.setItem('targetColor' , "#2196F3"); //if not set default
+    toolbarColors[2].innerHTML = "<i class=\"fas fa-check\"></i>";
   }
+
+
 }
 initCChanger();
+
+
+colors.addEventListener('click', (e) => { // if click get color from data-color attr
+  let color = e.target.getAttribute("data-color"); //if it is not null
+  if (color != null) {
+    ToolbarUncheck(toolbarColors);
+    localStorage.setItem('targetColor', color);
+    root.style.setProperty('--accent-color', color);
+    document.querySelector(`[data-color = "${localStorage.getItem('targetColor')}"]`).innerHTML = "<i class=\"fas fa-check\"></i>";
+  }
+});
+
+
+
+
+
+
+let cog = document.getElementById('cogRot');
+let toolbarColorsWrapper = document.getElementById('toolbarColors');
+cog.addEventListener('click', () => {
+  if (toolbarColorsWrapper.classList.contains('show_toolbar')) {
+    toolbarColorsWrapper.classList.remove('show_toolbar')
+  } else {
+    toolbarColorsWrapper.classList.add('show_toolbar');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+/*
+nav toggle
+ */
+
+let navToggle = document.getElementById('nav-toggle');
+let nav = document.querySelector('nav');
+navToggle.addEventListener('click', () => {
+  if (nav.classList.contains('active')) {
+    nav.classList.remove('active')
+  } else {
+    nav.classList.add('active')
+  }
+
+});
