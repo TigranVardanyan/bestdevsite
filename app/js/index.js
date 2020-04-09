c343b46135d588388f97b03e4d555abf1968c43b
Tigran <tig.vardanyan.001@gmail.com>
2020-04-10 02:36:22 +0400

    many problems

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,51 @@
 class AnimatedTransitions {
   pageOn;
-  pageOff;
-  constructor(pageOn, pageOff) {
+  pageOff = document.querySelector('section.current-section');
+  // sectionDefaultStyles = "visibility: hidden;\n" +
+  //   "      z-index: 90;\n" +
+  //   "      opacity: 0;\n" +
+  //   "      overflow: hidden;\n" +
+  //   "      height: 0;\n" +
+  //   "      position: absolute;\n" +
+  //   "      transform-origin: 0 0;\n" +
+  //   "      transform: rotate3d(0,0,0,0deg) translate3d(0,0,0) scale3d(1,1,1);";
+  constructor(pageOn) {
     this.pageOn = pageOn;
-    this.pageOff = pageOff;
   }
   getAnimationTypeNumber() {
     return Math.floor(Math.random()*7);
   }
-  animationType = {}
+  resetNavActiveClass(elements) {
+    for(let i = 0; i < elements.length; i++) {
+      elements[i].classList.remove('active')
+    }
+  }
+  showCurrentSection() {
+    // let sections = document.querySelectorAll('section.section')
+    // for(let i = 0; i < sections.length; i++) {
+    //   sections.classList.remove("current-section")
+    // }
+    this.pageOff.classList.add('animateOff');
+    this.pageOn.classList.add("animateOn");
+    this.pageOn.classList.add("current-section");
+    setTimeout(()=>{
+      this.pageOff.classList.remove('current-section');
+      this.pageOff.classList.remove('animateOff');
+      this.pageOn.classList.remove("animateOn");
+    }, 4000)
+  }
 }
+let navigationSection = document.querySelector('nav ul');
+let navigationSectionLi = document.querySelectorAll('nav ul li');
+navigationSection.addEventListener('click', (e)=> {
+  if(e.target.hasAttribute('data-section')) {
+    let pageOn = document.getElementById(e.target.getAttribute('data-section'));
+    console.log(e.toElement.parentElement);
+    let q = new AnimatedTransitions(pageOn);
+    q.resetNavActiveClass(navigationSectionLi);
+    e.toElement.parentElement.classList.add('active');
+    nav.classList.remove('active');
+    q.showCurrentSection();
+  }
+});
 
